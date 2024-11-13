570d2be5e50ee520b346b3eb80e41606586b6ecd
rkeeves <aprettydecentaddress@gmail.com>
2024-11-13 01:29:59 +0100

    just in case the input isnt trimmed

--- src/d11/d11.hs
+++ src/d11/d11.hs
@@ -4,22 +4,21 @@ import           Data.Maybe     (catMaybes)
 import           Data.Ord       (Down (..))
 
 main :: IO ()
-main = interact $ unlines . (\x -> ["A", a x, "B", b x]) . bimap sort sort . unzip . xysWhichAre (== '#') . lines
+main = interact $ unlines . (\x -> ["A", a x, "B", b x]) . bothmap sort . unzip . xysWhichAre (== '#') . lines
 
 a :: ([Int], [Int]) -> String
-a = show . uncurry (+) . bothmap (sumDistances . zipWithSkips (\x skips -> x + skips))
+a = show . uncurry (+) . bothmap (sumDistances . zipWithSkips (\x skips -> x + skips) 0)
 
 b :: ([Int], [Int]) -> String
-b = show . uncurry (+) . bothmap (sumDistances . zipWithSkips (\x skips -> x + skips * (1000000 - 1)))
+b = show . uncurry (+) . bothmap (sumDistances . zipWithSkips (\x skips -> x + skips * (1000000 - 1)) 0)
 
 bothmap f = bimap f f
 
 sumDistances :: [Int] -> Int
 sumDistances xs = let n = length xs in sum . zipWith (\i x -> (2 * i - n + 1) * x) [0 ..] $ xs
 
-zipWithSkips :: (Int -> Int -> Int) -> [Int] -> [Int]
-zipWithSkips _ [] = []
-zipWithSkips f xs = snd . mapAccumL (\(x, k) x' -> let k' = k + max (x' - x - 1) 0 in ((x', k'), f x' k')) (head xs, 0) $ xs
+zipWithSkips :: (Int -> Int -> Int) -> Int -> [Int] -> [Int]
+zipWithSkips f x0 = snd . mapAccumL (\(x, k) x' -> let k' = k + max (x' - x - 1) 0 in ((x', k'), f x' k')) (x0 - 1, 0)
 
 xysWhichAre :: (Char -> Bool) -> [[Char]] -> [(Int, Int)]
 xysWhichAre p css = concat [ catMaybes [ if p c then Just (x, y) else Nothing | (x, c) <- zip [0..] cs] | (y, cs) <- zip [0..] css]
